510af7dc9ddc4f9ec551da0775705dec1191cc15
zzz <zzz@zzzdeMacBook-Pro.local>
2026-04-01 12:32:21 +0800

    Add PNG icons and TWA manifest for GitHub Pages

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,13 @@
-const CACHE_NAME = "cute-habit-pet-cache-v1";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
+const CACHE_NAME = "cute-habit-pet-cache-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
